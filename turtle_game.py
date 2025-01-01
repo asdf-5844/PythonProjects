f6de93ca9e0eebbb6c5b3d2fe7c0ebe317c0625b
beneben <bchen5844@gmail.com>
2024-12-31 21:03:22 -0500

    Update turtle_game.py

--- turtle_game.py
+++ turtle_game.py
@@ -12,20 +12,20 @@ all_tur = []
 user_bet = screen.textinput("Enter your bet", "Which turtle color will win? (white, red, orange, yellow, blue, green, purple)")
 
 for index in range(0, 7):
-    new_tur = Turtle(shape="turtle")
-    new_tur.shapesize(2)
-    new_tur.speed('fastest')
-    new_tur.penup()
-    new_tur.goto(x=-350, y=y_positions[index])
-    new_tur.color(colors[index])
-    all_tur.append(new_tur)
+    new_tur = Turtle(shape="turtle") #turtle shape
+    new_tur.shapesize(2) #Increase size
+    new_tur.speed('fastest') #Set animation speed to fastest
+    new_tur.penup() #Lifts pen so it doesn't draw lines
+    new_tur.goto(x=-350, y=y_positions[index]) #Go to starting positions
+    new_tur.color(colors[index]) #Assign colors
+    all_tur.append(new_tur) #Adds the newly created turtle for later use.
 
 is_on = True
 while is_on:
     for turtle in all_tur:
         if turtle.xcor() > 330:
-            is_on = False
-            winner = turtle.pencolor()
+            is_on = False #Stops the race
+            winner = turtle.pencolor() #Identifies winning turtle
             if winner == user_bet:
                 turtle.write("You won! " + winner + " turtle is the winner.", font=("Courier", 16, "normal"),
                              align="right")
