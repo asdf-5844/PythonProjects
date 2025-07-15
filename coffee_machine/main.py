42affe5a5a2c81b00f5d3400388a8587531eb82f
beneben <bchen5844@gmail.com>
2025-07-15 13:51:30 -0400

    Update main.py

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -30,3 +30,62 @@ resources = {
     "coffee": 100,
 }
 
+profit = 0
+
+
+def process_coins():
+    """Returns the total calculated from coins inserted."""
+    print("Please insert coins.")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.10
+    total += int(input("how many nickles?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    return round(total, 2)
+
+
+def is_resource_sufficient(order_ingredients):
+    """Returns True when order can be made, False if ingredients are insufficient."""
+    for item in order_ingredients:
+        if order_ingredients[item] > resources.get(item, 0):
+            print(f"Sorry, there is not enough {item}.")
+            return False
+    return True
+
+
+def print_report():
+    """Prints the resources in an organized format"""
+    print(f"Water: {resources['water']}ml")
+    print(f"Milk: {resources.get('milk', 0)}ml")
+    print(f"Coffee: {resources['coffee']}g")
+    print(f"Money: ${profit:.2f}")
+
+
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the required ingredients from the resources."""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}. Enjoy!")
+
+
+on = True
+while on:
+    choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+    if choice == "off":
+        on = False
+    elif choice == "report":
+        print_report()
+    elif choice in MENU:
+        drink = MENU[choice]
+        if is_resource_sufficient(drink["ingredients"]):
+            payment = process_coins()
+            if payment < drink["cost"]:
+                print("Sorry, that's not enough money. Money refunded.")
+            else:
+                change = round(payment - drink["cost"], 2)
+                if change > 0:
+                    print(f"Here is ${change} in change.")
+                profit += drink["cost"]
+                make_coffee(choice, drink["ingredients"])
+    else:
+        print("Invalid selection.")
